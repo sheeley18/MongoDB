7840dcbf99fbc2a917198bfd3198bf5c05ca5f0b
Sean Heeley <67065230+sheeley18@users.noreply.github.com>
2025-08-12 10:44:24 -0400

    Update sg.tf
    Locked down further

--- sg.tf
+++ sg.tf
@@ -1,43 +1,30 @@
-# Security Group for EC2 instance
-resource "aws_security_group" "terraform_sg" { 
-  name        = "allow_ssh_mongo"
-  description = "Security group to allow SSH and MongoDB access"
+# Security Group for MongoDB EC2 instance
+resource "aws_security_group" "mongodb_sg" { 
+  name        = "mongodb_secure_sg"
+  description = "Security group for MongoDB - allows SSH and VPC access only"
   vpc_id      = aws_vpc.mongo_vpc.id   
   
   tags = {
-    Name = "Terraform_project_sg"
+    Name = "MongoDB_Secure_SG"
   }
 }
 
-# SSH access rule
-resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {  
-  security_group_id = aws_security_group.terraform_sg.id  
-  cidr_ipv4         = "73.234.1.227/32"  # Your IP only, not 0.0.0.0/0
+# SSH access rule - ONLY from admin IP
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_admin" {  
+  security_group_id = aws_security_group.mongodb_sg.id  
+  cidr_ipv4         = var.admin_ip
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
   
   tags = {
-    Name = "SSH_Access_MyIP"
+    Name = "SSH_Access_Admin"
   }
 }
 
-# MongoDB access rule - SECURED: Only allow access from your IP
-resource "aws_vpc_security_group_ingress_rule" "allow_mongo_my_ip" {
-  security_group_id = aws_security_group.terraform_sg.id
-  cidr_ipv4         = "73.234.1.227/32"  # Your home network IP
-  from_port         = 27017
-  ip_protocol       = "tcp"
-  to_port           = 27017
-  
-  tags = {
-    Name = "MongoDB_Access_MyIP"
-  }
-}
-
-# OPTIONAL: MongoDB access from within VPC (keep for internal access)
+# MongoDB access from VPC - will be used by EKS
 resource "aws_vpc_security_group_ingress_rule" "allow_mongo_vpc" {
-  security_group_id = aws_security_group.terraform_sg.id
+  security_group_id = aws_security_group.mongodb_sg.id
   cidr_ipv4         = "192.168.0.0/16"  # VPC CIDR block
   from_port         = 27017
   ip_protocol       = "tcp"
@@ -49,19 +36,18 @@ resource "aws_vpc_security_group_ingress_rule" "allow_mongo_vpc" {
 }
 
 # Egress rule - allow all outbound traffic
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.terraform_sg.id
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
+  security_group_id = aws_security_group.mongodb_sg.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"  # All protocols and ports
+  ip_protocol       = "-1"
   
   tags = {
     Name = "All_Outbound"
   }
 }
 
-# Output security group ID for use in other modules
-output "security_group_id" {
-  description = "ID of the security group"
-  value       = aws_security_group.terraform_sg.id
+# Output security group ID for EKS repo to reference
+output "mongodb_security_group_id" {
+  description = "Security group ID for MongoDB instance"
+  value       = aws_security_group.mongodb_sg.id
 }
-
