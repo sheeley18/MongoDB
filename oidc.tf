155d39fae7168c069e054577d5ee3194dca554bf
Sean Heeley <67065230+sheeley18@users.noreply.github.com>
2025-08-12 20:21:15 -0400

    Update oidc.tf

--- oidc.tf
+++ oidc.tf
@@ -55,7 +55,7 @@ resource "aws_iam_role" "github_actions_eks" {
           "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
         }
         StringLike = {
-          "token.actions.githubusercontent.com:sub" = "repo:sheeley18/infrastructure-eks:*"
+          "token.actions.githubusercontent.com:sub" = "repo:sheeley18/tasky-eks-cluster:*"
         }
       }
     }]
